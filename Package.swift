84bcdeebd8ffc5fe646df8a51244577bb38abe03
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-19 15:53:33 +0200

    chore: Add SwiftTUI

--- Package.swift
+++ Package.swift
@@ -5,9 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "hocusfocus",
+    dependencies: [
+        .package(url: "https://github.com/rensbreur/SwiftTUI", branch: "main")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "hocusfocus"
         ),
